29af2bf8bdf1edc99f0f491740385905ec37f212
Dominik Wrona <dwrona92@gmail.com>
2025-11-14 12:11:54 +0100

    update_commit

--- backend-dotnet/Fro.Infrastructure/obj/Debug/net8.0/Fro.Infrastructure.AssemblyInfo.cs
+++ backend-dotnet/Fro.Infrastructure/obj/Debug/net8.0/Fro.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fro.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a63d1c67b132ccae47477607213302e25186fe35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+751742f604ca9d18008fa96af3b1b4d39ce3ac9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fro.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fro.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
